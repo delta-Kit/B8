7723265e5d8b4433e6fe5d09ce70cd73c139fe0c
unknown <fuki.kitsukawa@gmail.com>
2021-07-05 15:53:34 +0900

    液アメーバのmsdの計算、フラクタル次元のコードの改良

--- ameba/ameba_analysis/fd.cpp
+++ ameba/ameba_analysis/fd.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <vector>
 #include <math.h>
 
@@ -9,32 +7,31 @@ using namespace std;
 int main(){
     //amebaTxt：num = 982, 234 * 235
     //amebaTxt2：num = 1001, 344 * 345
-    const int num = 982, H = 234, W = 235, boxNum = 7;
-    ifstream readingFile;
+    const int num = 982, H = 220, W = 235, boxNum = 7;
     for(int i = 0; i < num; i++){
-        vector<vector<int>> isAmeba(H, vector<int>(W));
+        vector<vector<int>> isAmeba(H, vector<int>(W)), sum(H + 5, vector<int>(W + 5));
         int two = 1;
         double xySum = 0, xSum = 0, ySum = 0, xSqSum = 0;
-        readingFile.open("../amebaTxt/ameba" + to_string (i / 1000) + to_string((i / 100) % 10) + to_string((i / 10) % 10) + to_string(i % 10) + ".txt", ios::in);
+        char fileName[] = "../amebaTxt/ameba0000.txt";
+        fileName[17] = i / 1000 + '0';
+        fileName[18] = (i / 100) % 10 + '0';
+        fileName[19] = (i / 10) % 10 + '0';
+        fileName[20] = i % 10 + '0';
+        FILE *fp = NULL;
+        fp = fopen(fileName, "r");
         for(int j = 0; j < H; j++){
-            for(int k = 0; k < W; k++)readingFile >> isAmeba[j][k];
+            for(int k = 0; k < W; k++){
+                fscanf(fp, "%d", &isAmeba[j][k]);
+                isAmeba[j][k] = min(1, isAmeba[j][k]);
+                sum[j + 1][k + 1] = sum[j][k + 1] + sum[j + 1][k] - sum[j][k] + isAmeba[j][k];
+            }
         }
-        readingFile.close();
+        fclose(fp);
         for(int j = 0; j < boxNum; j++){
             int cnt = 0;
-            for(int k = 0; k < W - two; k += two){
-                for(int l = 0; l < H - two; l += two){
-                    bool exist = false;
-                    for(int m = k; m < k + two; m++){
-                        for(int n = l; n < l + two; n++){
-                            if(isAmeba[m][n]){
-                                exist = true;
-                                break;
-                            }
-                        }
-                        if(exist)break;
-                    }
-                    if(exist)cnt++;
+            for(int k = 0; k < H - two; k += two){
+                for(int l = 0; l < W - two; l += two){
+                    if(sum[k + two][l + two] - sum[k][l + two] - sum[k + two][l] + sum[k][l])cnt++;
                 }
             }
             double x = log(two), y = log(cnt);
