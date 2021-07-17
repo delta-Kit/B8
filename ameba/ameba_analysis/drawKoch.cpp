658f84cba216d35caab2717b0881169d8be54773
unknown <fuki.kitsukawa@gmail.com>
2021-07-17 23:13:22 +0900

    中間発表段階

---
+++ ameba/ameba_analysis/drawKoch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    const int N = 6;
+    queue<pair<double, double>> q;
+    int n4 = 1;
+    for(int i = 0; i < N; i++)n4 *= 4;
+    q.push({0, 0});
+    q.push({800, 0});
+    for(int i = 0; i < (n4 - 1) / 3; i++){
+        
+    }
+    return 0;
+}
--- ameba/ameba_analysis/fd.cpp
+++ ameba/ameba_analysis/fd.cpp
@@ -5,13 +5,11 @@
 using namespace std;
 
 int main(){
-    //amebaTxt：num = 982, 234 * 235
-    //amebaTxt2：num = 1001, 344 * 345
-    const int num = 982, H = 220, W = 235, boxNum = 7;
+    int num, H, W, boxNum = 0;
     for(int i = 0; i < num; i++){
-        vector<vector<int>> isAmeba(H, vector<int>(W)), sum(H + 5, vector<int>(W + 5));
         int two = 1;
         double xySum = 0, xSum = 0, ySum = 0, xSqSum = 0;
+        //ファイル読み込み
         char fileName[] = "../amebaTxt/ameba0000.txt";
         fileName[17] = i / 1000 + '0';
         fileName[18] = (i / 100) % 10 + '0';
@@ -19,14 +17,21 @@ int main(){
         fileName[20] = i % 10 + '0';
         FILE *fp = NULL;
         fp = fopen(fileName, "r");
+        if(!i){
+            fscanf(fp, "%d %d %d", &num, &H, &W);
+            boxNum = log2(min(H, W)) + 1;
+        }
+        vector<vector<int>> isAmeba(H, vector<int>(W)), sum(H + 5, vector<int>(W + 5));
         for(int j = 0; j < H; j++){
             for(int k = 0; k < W; k++){
                 fscanf(fp, "%d", &isAmeba[j][k]);
                 isAmeba[j][k] = min(1, isAmeba[j][k]);
+                //2次元累積和の計算
                 sum[j + 1][k + 1] = sum[j][k + 1] + sum[j + 1][k] - sum[j][k] + isAmeba[j][k];
             }
         }
         fclose(fp);
+        //ボックスカウント法
         for(int j = 0; j < boxNum; j++){
             int cnt = 0;
             for(int k = 0; k < H - two; k += two){
@@ -34,6 +39,7 @@ int main(){
                     if(sum[k + two][l + two] - sum[k][l + two] - sum[k + two][l] + sum[k][l])cnt++;
                 }
             }
+            //最小二乗法
             double x = log(two), y = log(cnt);
             xySum += x * y;
             xSum += x;
@@ -41,6 +47,7 @@ int main(){
             xSqSum += x * x;
             two *= 2;
         }
+        //時刻とフラクタル次元を空白区切りで出力
         printf("%f %f\n", (double)i / 30, -(boxNum * xySum - xSum * ySum) / (boxNum * xSqSum - xSum * xSum));
     }
     return 0;
